a3b87fafc4cfd82629794af797fe19232b9c6419
Carlos Henrique Crisostomo Reis <carloshenriquecreis@gmail.com>
2025-12-15 11:48:53 -0300

    Part 8 - Config Home Page

--- por/src/App.js
+++ por/src/App.js
@@ -1,6 +1,6 @@
 
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import './App.scss';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import Home from './containers/home';
 import About from './containers/about';
 import Resume from './containers/resume';
@@ -12,23 +12,30 @@ import ParticleBackground from './particles';
 
 
 function App() {
+  const location = useLocation();
+  console.log("Current location:", location.pathname);
+
+  const renderParticleJsHomePage = location.pathname === '/home' || location.pathname === '/';
 
   return (
     <div className="App">
         {/* particles js */}
-        <ParticleBackground/>
+        {
+          renderParticleJsHomePage && ( <ParticleBackground/> )
+        }
         {/* nav bar */}
         <NavBar />
         {/* main page content */}
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/resume' element={<Resume/>} />
-          <Route path='/skills' element={<Skills/>} />
-          <Route path='/portfolio' element={<Portfolio/>} />
-          <Route path='/contact' element={<Contact/>} />
-
-        </Routes>
+        <div className='App_main-page-content'>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/resume' element={<Resume/>} />
+            <Route path='/skills' element={<Skills/>} />
+            <Route path='/portfolio' element={<Portfolio/>} />
+            <Route path='/contact' element={<Contact/>} />
+          </Routes>
+        </div>
     </div>
   );
 }
--- por/src/containers/home/index.jsx
+++ por/src/containers/home/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-
+import './styles.scss';
 const Home = () =>{
     return (
-        <div>
-            Home Page
-        </div>
+        <section id="home" className="home">
+            <div className="home_text-wrapper">
+                <h1>
+                    Olá, meu é Carlos Henrique
+                    <br />
+                    Sou um Desenvolvedor Full Stack
+                </h1>
+            </div>
+
+        </section>
     );
 };
 export default Home;
