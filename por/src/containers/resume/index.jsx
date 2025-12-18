5d79958d105f5ab7abcc70881f73378650e4aae8
Carlos Henrique Crisostomo Reis <carloshenriquecreis@gmail.com>
2025-12-18 12:11:41 -0300

    Part 14: Resume (error)

--- por/src/containers/resume/index.jsx
+++ por/src/containers/resume/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import {BsInfoCircleFill} from "react-icons/bs"
 import PageHeaderContent from "../../components/pageHeaderContent";
+import {
+    VerticalTimeline,
+    VerticalTimelineElement} from "react-vertical-timeline-component";
+import'react-vertical-timeline-component/style.min.css'  ;
+import { data } from "./util";
+
+
 
 const Resume = () =>{
     return (
@@ -10,6 +17,41 @@ const Resume = () =>{
                 headerText ="My Resume"
                 icon={<BsInfoCircleFill size={40} />}
                 />
+                <div className="timeLine">
+                    <div className="timeLine_experience">
+                        <h3 className="timeLine_experience_header-text">Experiencias</h3>
+                        <VerticalTimeline
+                        layout = {'1-column'}
+                        lineColor = "var(--yellow-theme-main-color)"
+                        >
+                            {
+                                data.experience.map((item, i) => (
+                                    <VerticalTimelineElement
+                                    key={i}
+                                    className = "timeLine_experience_vertical-timeline-element"
+                                    >
+                                        <div className="vertical-timeline-element-title-wrapper">
+                                            <h3 className="vertical-timeline-element-title">
+                                                {item.title}
+                                            </h3>
+
+                                            <h4 className="vertical-timeline-element-subtitle">
+                                                {item.subTitle}
+                                            </h4>
+
+                                            <p>{item.description}</p>
+
+                                        </div>
+                                    </VerticalTimelineElement>
+                                ))
+                            }
+                        </VerticalTimeline>
+                    </div>
+                    <div className="timeLine_education">
+                        <h3 className="timeLine_education_header-text">Certificações</h3>
+                    </div>
+                </div>
+
             </section>
         </div>
     );
---
+++ por/src/containers/resume/util.js
@@ -0,0 +1,21 @@
+export const data = {
+    experience: [
+        {
+            title: "Aspirante a Oficial",
+            subTitle: "Brasília, Brasil",
+            description: "Atividades militares"
+        },
+        {
+            title: "Suporte Técnico N1",
+            subTitle: "Brasília, Brasil",
+            description: "Suporte técnico em computadores"
+        },
+    ],
+    education: [
+        {
+            title: "Análise e Desenvolvimento de Sistemas",
+            subTitle: "Universidade Cruzeiro do Sul",
+            description: "Técnologo"
+        },
+    ],
+}
