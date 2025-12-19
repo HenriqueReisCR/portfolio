e723eb4ddec23b868fcbe65e2a1c59a4aca57a49
Carlos Henrique Crisostomo Reis <carloshenriquecreis@gmail.com>
2025-12-19 11:57:18 -0300

    Past 14 - Resumo
    error corrigido

--- por/src/containers/resume/index.jsx
+++ por/src/containers/resume/index.jsx
@@ -3,7 +3,8 @@ import {BsInfoCircleFill} from "react-icons/bs"
 import PageHeaderContent from "../../components/pageHeaderContent";
 import {
     VerticalTimeline,
-    VerticalTimelineElement} from "react-vertical-timeline-component";
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import'react-vertical-timeline-component/style.min.css'  ;
 import { data } from "./util";
 
@@ -17,9 +18,9 @@ const Resume = () =>{
                 headerText ="My Resume"
                 icon={<BsInfoCircleFill size={40} />}
                 />
-                <div className="timeLine">
-                    <div className="timeLine_experience">
-                        <h3 className="timeLine_experience_header-text">Experiencias</h3>
+                <div className="timeline">
+                    <div className="timeline_experience">
+                        <h3 className="timeline_experience_header-text">Experiencias</h3>
                         <VerticalTimeline
                         layout = {'1-column'}
                         lineColor = "var(--yellow-theme-main-color)"
@@ -28,7 +29,7 @@ const Resume = () =>{
                                 data.experience.map((item, i) => (
                                     <VerticalTimelineElement
                                     key={i}
-                                    className = "timeLine_experience_vertical-timeline-element"
+                                    className = "timeline_experience_vertical-timeline-element"
                                     >
                                         <div className="vertical-timeline-element-title-wrapper">
                                             <h3 className="vertical-timeline-element-title">
@@ -47,8 +48,34 @@ const Resume = () =>{
                             }
                         </VerticalTimeline>
                     </div>
-                    <div className="timeLine_education">
-                        <h3 className="timeLine_education_header-text">Certificações</h3>
+                    <div className="timeline_education">
+                        <h3 className="timeline_education_header-text">Certificações</h3>
+                            <VerticalTimeline
+                            layout = {'1-column'}
+                            lineColor = "var(--yellow-theme-main-color)"
+                            >
+                                {
+                                data.education.map((item, i) => (
+                                    <VerticalTimelineElement
+                                    key={i}
+                                    className = "timeline_experience_vertical-timeline-element"
+                                    >
+                                        <div className="vertical-timeline-element-title-wrapper">
+                                            <h3 className="vertical-timeline-element-title">
+                                                {item.title}
+                                            </h3>
+
+                                            <h4 className="vertical-timeline-element-subtitle">
+                                                {item.subTitle}
+                                            </h4>
+
+                                            <p>{item.description}</p>
+
+                                        </div>
+                                    </VerticalTimelineElement>
+                                ))
+                            }
+                    </VerticalTimeline>
                     </div>
                 </div>
 
